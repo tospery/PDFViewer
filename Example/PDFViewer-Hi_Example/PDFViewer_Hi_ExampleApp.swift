fac1a8553f3ad61be028da11e663e7e2285ba656
tospery <tospery@gmail.com>
2024-12-17 04:06:49 +0800

    1.0.2.1

---
+++ Example/PDFViewer-Hi_Example/PDFViewer_Hi_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDFViewer_Hi_ExampleApp.swift
+//  PDFViewer-Hi_Example
+//
+//  Created by 杨建祥 on 2024/12/17.
+//
+
+import SwiftUI
+
+@main
+struct PDFViewer_Hi_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
